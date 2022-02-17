386f16db018b2ca09f4512f42dcbd0735b7c0ea7
DMB29 <mbilaldogan90@gmail.com>
2022-02-17 12:58:33 +0300

    visible

--- js/script.js
+++ js/script.js
@@ -11,16 +11,18 @@ const container = document.querySelector(".container")
 // sayfamı yüklediğim zaman bekleme işareti olan spinner.gif göstermek istiyorum.ilk başta dom ile yakalamalıyım. burada öncelikle loading başlatırım ve sonra loading i sona erdiririm.
 const loading = document.getElementById("loading");
 window.addEventListener("load" , () =>{
-    loading.style.display ="block" ; // görünür yaptım
+    loading.style.visibility ="visible" ; // görünür yaptım
 
     // sayfa yüklendiği zaman bir saniye görünür yapmalıyım
     // setTimeout = gecikmeli başlatmak.1.fonk,2.gecikme milisn
     // setInterval =belirli periyotlarla çalıştırmak
     setTimeout(() => {
         loading.style.display ="none"
+
+        //loading den sonra yazılarımı göster
+        container.style.display="flex" ;
     } , 1000)
-    //loading den sonra yazılarımı göster
-    container.style.display="flex" ;
+
 })
 
 //loading bittikten sonra sayfamda container divimi ekranda göstermke istiyorum.ilkin DOM ile yakalamalıyım

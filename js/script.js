e496be9b491e6ef967949b0f1cc623d026ae8dca
DMB29 <mbilaldogan90@gmail.com>
2022-02-17 12:50:20 +0300

    date

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// dom iile tüm elementlerimi yakalıyorum
+const years = document.getElementById("years") ;
+const months = document.getElementById("months") ;
+const days = document.getElementById("days") ;
+const hours = document.getElementById("hours") ;
+const minutes = document.getElementById("minutes") ;
+const seconds = document.getElementById("seconds"); 
+
+const container = document.querySelector(".container")
+
+// sayfamı yüklediğim zaman bekleme işareti olan spinner.gif göstermek istiyorum.ilk başta dom ile yakalamalıyım. burada öncelikle loading başlatırım ve sonra loading i sona erdiririm.
+const loading = document.getElementById("loading");
+window.addEventListener("load" , () =>{
+    loading.style.display ="block" ; // görünür yaptım
+
+    // sayfa yüklendiği zaman bir saniye görünür yapmalıyım
+    // setTimeout = gecikmeli başlatmak.1.fonk,2.gecikme milisn
+    // setInterval =belirli periyotlarla çalıştırmak
+    setTimeout(() => {
+        loading.style.display ="none"
+    } , 1000)
+    //loading den sonra yazılarımı göster
+    container.style.display="flex" ;
+})
+
+//loading bittikten sonra sayfamda container divimi ekranda göstermke istiyorum.ilkin DOM ile yakalamalıyım
+
+
+// uzun yol olarak istersem innertext ile içine ulaşabilirim.
+
+years.innerText = "00";
+years.innerText = "00";
+years.innerText = "00";
+years.innerText = "00";
+years.innerText = "00";
+
+
